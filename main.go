b5d152bffde4eddbef26bd66fa5b205aa1dbc792
beastthor <beastthor.ykc@gmail.com>
2022-10-04 12:46:51 -0400

    3rd commit

--- main.go
+++ main.go
@@ -3,14 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	//maps representation 2
+	//var colors map[string]string
 
-	var colors map[string]string
+	//maps represntation 3
+	colors := make(map[int]string)
 
+	//maps represntation 1
 	//colors := map[string]string{
 
 	//	"red":   "#ff0000",
 	//	"green": "#4bf745",
 	//}
+
+	//Asinging a value to map after creating it
+	colors[10] = "#ffffff"
 
 	fmt.Println(colors)
 
